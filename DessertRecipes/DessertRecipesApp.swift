dbc17e00a113c9fc2b00d341b5922ed6ad96350c
Rahul <rahul.n.sharma511@gmail.com>
2024-02-02 13:06:00 -0800

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Rahul Sharma on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
